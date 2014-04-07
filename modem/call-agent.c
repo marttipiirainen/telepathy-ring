c58b51519aafd9f8bee1c76d1a32149243587742
Martti Piirainen <martti.piirainen@oss.tieto.com>
2014-04-07 10:00:48 +0300

    [call] more ringback tone improvements

--- modem/call-agent.c
+++ modem/call-agent.c
@@ -31,7 +31,6 @@
 #include "modem/request-private.h"
 
 #include <dbus/dbus-glib-lowlevel.h>
-//#include "gdbus.h" // Todo: This was not compiling
 
 #include <string.h>
 #include <errno.h>
@@ -40,13 +39,14 @@
 
 G_DEFINE_TYPE (ModemCallAgent, modem_call_agent, MODEM_TYPE_OFACE);
 
-/* Todo: not sure what path should be like */
-#define VOICECALL_UNSOLICITED_AGENT_PATH  "/voicecallagent"
+#define VOICECALL_AGENT_PATH  "/voicecallagent"
 
 struct _ModemCallAgentPrivate
 {
   gboolean agentServiceRegistered;
   DBusGProxy *agentProxy;
+  ModemTones *tones;
+  guint playing_tone;
 };
 
 /* ---------------------------------------------------------------------- */
@@ -78,17 +78,15 @@ modem_call_agent_init (ModemCallAgent *self)
   self->priv = G_TYPE_INSTANCE_GET_PRIVATE(self,
       MODEM_TYPE_CALL_AGENT, ModemCallAgentPrivate);
       
-  /* Todo: I don't know if we would need some initialisations for
-   * the interface and proxy different when acting as agent */
-
-  /* e.g. If we would provide interface */
   self->priv->agentProxy =
     dbus_g_proxy_new_for_name(dbus_g_bus_get(DBUS_BUS_SYSTEM, NULL),
       MODEM_OFACE_CALL_AGENT,
-      VOICECALL_UNSOLICITED_AGENT_PATH,
+      VOICECALL_AGENT_PATH,
       MODEM_OFACE_CALL_AGENT);
 
   self->priv->agentServiceRegistered = FALSE;
+  self->priv->tones = 0;
+  self->priv->playing_tone = 0;
 }
 
 static void
@@ -103,6 +101,142 @@ modem_call_agent_finalize (GObject *object)
 {
   G_OBJECT_CLASS (modem_call_agent_parent_class)->finalize (object);
 }
+
+/* ---------------------------------------------------------------------- */
+/* ModemCallAgent interface */
+
+
+
+static void
+call_agent_play_cb(ModemTones *tones,
+  guint source,
+  gpointer _self)
+{
+  ModemCallAgent *self;
+  DEBUG("play tone cb");
+
+  if (_self) {
+	  /* TODO check if needed */
+	  self = MODEM_CALL_AGENT(_self);
+	  g_object_unref(self);
+  }
+}
+
+
+void
+ringback_tone (DBusConnection *conn,
+                                  DBusMessage *msg, void *data)
+{
+  gboolean playTone;
+  DBusMessageIter iter;
+  ModemCallAgent *self = data;
+  DEBUG ("###piiramar### got it");
+
+  dbus_message_iter_init(msg, &iter);
+  if (dbus_message_iter_get_arg_type(&iter) != DBUS_TYPE_BOOLEAN)
+		DEBUG ("Invalid arguments received");
+
+  dbus_message_iter_get_basic(&iter, &playTone);
+
+  DEBUG ("%d", (int) playTone);
+
+  /* Todo: Should we check here the call state first before
+   * just playing the tone? If we would be able to get call
+   * state from call-service e.g.
+   * if (state == MODEM_CALL_STATE_ALERTING && strcmp(type, "outgoing") == 0)
+   */
+
+  if (!self->priv->tones) {
+	  /* Lazy init (this is a rare feature, after all) */
+	  gpointer tones_object = g_object_new( MODEM_TYPE_TONES,
+			  NULL );
+	  DEBUG("### tones_object=%p", tones_object);
+	  self->priv->tones = MODEM_TONES(tones_object);
+	  DEBUG("### tones=%p", self->priv->tones);
+  }
+
+  if (!self->priv->tones) {
+	  DEBUG("Cannot play tones.");
+	  return;
+  }
+
+
+  if (playTone == TRUE)
+    {
+    DEBUG("Play local tone");
+
+    if (!modem_tones_is_playing(self->priv->tones, self->priv->playing_tone)){
+    	self->priv->playing_tone = modem_tones_start_full(self->priv->tones,
+    			TONES_EVENT_RINGING, 0, 0,
+    			call_agent_play_cb,
+    	        g_object_ref(self));
+    	DEBUG("playing, source = %d", self->priv->playing_tone);
+    }
+
+    /*ring_media_channel_play_tone(RING_MEDIA_CHANNEL(self),
+      TONES_EVENT_RINGING, 0, 0);*/
+    }
+  else {
+  	DEBUG("playing, source = %d", self->priv->playing_tone);
+	modem_tones_stop(self->priv->tones, self->priv->playing_tone);
+    /*ring_media_channel_stop_playing(RING_MEDIA_CHANNEL(self), TRUE);*/
+  }
+
+}
+
+static DBusHandlerResult call_agent_generic_dbus_message(DBusConnection *conn,
+						DBusMessage *msg)
+{
+	DBusMessage *reply;
+
+	reply = dbus_message_new_method_return(msg);
+	if (!reply)
+		return DBUS_HANDLER_RESULT_NEED_MEMORY;
+
+	dbus_connection_send(conn, reply, NULL);
+	dbus_message_unref(reply);
+
+	return DBUS_HANDLER_RESULT_HANDLED;
+}
+
+static DBusHandlerResult call_agent_dbus_message_handler(DBusConnection *conn,
+				DBusMessage *msg, void *user_data)
+{
+	const char *method = dbus_message_get_member(msg);
+	const char *iface = dbus_message_get_interface(msg);
+
+	/*if ((strcmp("Introspect", method) == 0) &&
+		(strcmp("org.freedesktop.DBus.Introspectable", iface) == 0))
+		return introspect(conn, msg);*/
+
+	if (strcmp(MODEM_OFACE_CALL_AGENT, iface) != 0)
+		return DBUS_HANDLER_RESULT_NOT_YET_HANDLED;
+
+	if (strcmp("Release", method) == 0) {
+		DEBUG("release");
+		/* TODO unregister etc. */
+		return call_agent_generic_dbus_message(conn, msg);
+	}
+	else if (strcmp("RingbackTone", method) == 0) {
+		DEBUG("ringback");
+		ringback_tone(conn, msg, user_data);
+		return call_agent_generic_dbus_message(conn, msg);
+	}
+
+	return DBUS_HANDLER_RESULT_NOT_YET_HANDLED;
+}
+
+static void call_agent_unregister(DBusConnection *connection, void *user_data)
+{
+	DEBUG("");
+}
+
+
+static DBusObjectPathVTable call_agent_table = {
+		.unregister_function	= call_agent_unregister,
+		.message_function	= call_agent_dbus_message_handler,
+};
+
 
 /* ---------------------------------------------------------------------- */
 
@@ -125,13 +259,24 @@ modem_call_agent_connect (ModemOface *_self)
     DEBUG ("###piiramar### proxy=%p path=%s bus=%s interface=%s"
     		, proxy, dbus_g_proxy_get_path (proxy), dbus_g_proxy_get_bus_name(proxy), dbus_g_proxy_get_interface (proxy));
 
-    /* Register call agent in order to receive requests for playing local tones 
+    /* Register call agent in order to receive requests for playing local tones
     */
     dbus_g_proxy_call_no_reply(proxy, "RegisterVoicecallAgent",
     		DBUS_TYPE_G_OBJECT_PATH, dbus_g_proxy_get_path(self->priv->agentProxy),
     		G_TYPE_INVALID);
-    /* Todo: Check also the error value if we need more information about fail */
-    /*if (request)*/
+    /* TODO error case */
+
+    DEBUG ("###piiramar### agent reg done");
+
+    dbus_connection_register_object_path(
+    		dbus_bus_get (DBUS_BUS_SYSTEM, NULL),
+    		VOICECALL_AGENT_PATH,
+    		&call_agent_table,
+    		_self);
+    /* TODO error case */
+
+    DEBUG ("###piiramar### object reg done");
+
     priv->agentServiceRegistered = TRUE;
     }
 }
@@ -149,14 +294,17 @@ modem_call_agent_disconnect (ModemOface *_self)
 
   /* Unregister Voicecall Agent if we are registered */
   if (priv->agentServiceRegistered)
-    ret = dbus_g_proxy_call (proxy, "UnregisterVoiceCallManagerAgent", NULL,
+	dbus_connection_unregister_object_path(
+			dbus_bus_get (DBUS_BUS_SYSTEM, NULL),
+			VOICECALL_AGENT_PATH);
+  /* TODO error case */
+  ret = dbus_g_proxy_call (proxy, "UnregisterVoiceCallManagerAgent", NULL,
     		DBUS_TYPE_G_OBJECT_PATH, dbus_g_proxy_get_path(self->priv->agentProxy),
             G_TYPE_INVALID,
             G_TYPE_INVALID);
-                                
-  /* Todo: Do we even need to care or should we just use dbus_g_proxy_send? */
-  if (ret)
-    priv->agentServiceRegistered = FALSE;
+
+  /* TODO error case */
+  priv->agentServiceRegistered = FALSE;
 }
 
 /* ---------------------------------------------------------------------- */
@@ -182,45 +330,6 @@ modem_call_agent_class_init (ModemCallAgentClass *klass)
   DEBUG ("leave");
 }
 
-/* ---------------------------------------------------------------------- */
-/* ModemCallAgent interface */
-
-/* Todo: This is completely in phase still. Don't understand how the oFono
- * is able to call the method here via path. Should we introduce methods
- * as I commented below? */
-void
-RingbackTone (DBusConnection *conn,
-                                  DBusMessage *msg, void *data)
-{
-  gboolean *playTone;
-  DBusMessageIter iter;
-  struct ModemCallAgent *self = data;
-
-  if (dbus_message_iter_get_arg_type(&iter) != DBUS_TYPE_BOOLEAN)
-		DEBUG ("Invalid arguments received");
-
-  dbus_message_iter_get_basic(&iter, &playTone);
-
-  DEBUG ("%d", (int) playTone);
-
-  /* Todo: Should we check here the call state first before
-   * just playing the tone? If we would be able to get call
-   * state from call-service e.g.
-   * if (state == MODEM_CALL_STATE_ALERTING && strcmp(type, "outgoing") == 0) 
-   */
-
-  if (*playTone == TRUE)
-    {
-    DEBUG("Play local tone");
-    // Play local alert tone here
-    ring_media_channel_play_tone(RING_MEDIA_CHANNEL(self),
-      TONES_EVENT_RINGING, 0, 0);
-    }
-  else
-    ring_media_channel_stop_playing(RING_MEDIA_CHANNEL(self), TRUE);
-
-}
-
 /* Todo: This was not compiling yet */
 /*void
 Release (DBusConnection *conn, DBusMessage *msg, void *data)
@@ -234,9 +343,3 @@ Release (DBusConnection *conn, DBusMessage *msg, void *data)
   priv->agentServiceRegistered = FALSE;
 }*/
 
-/*static const GDBusMethodTable call_agent_methods[] = {
-	{ GDBUS_METHOD("RingbackTone", GDBUS_ARGS({ "playTone", "a{b}" }), NULL,
-			modem_call_agent_ringback_tone) },
-	{ GDBUS_METHOD("Release", NULL, NULL, modem_call_agent_release) },
-	{ }
-};*/
